d520389e0551b58663c1ff0f2e457fa08e88d2aa
Keith Carlos <108279070+Kratos3434@users.noreply.github.com>
2024-01-15 18:48:06 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server';
 
-// const protectedRoutes = ["/", "/:name"];
+const protectedRoutes = ["/", "/:name"];
 
-// export default async function middleware(req: NextRequest) {
-//     const token = req.cookies.get('token')?.value;
+export default async function middleware(req: NextRequest) {
+    const token = req.cookies.get('token')?.value;
 
-//     const res = await fetch("http://localhost:8080/admin/user/authenticate", {
-//             method: "GET",
-//             headers: {
-//                 "Content-Type": 'application/json',
-//                 "Authorization": `Bearer ${token}`
-//             }
-//         });
+    const res = await fetch("http://localhost:8080/admin/user/authenticate", {
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json',
+                "Authorization": `Bearer ${token}`
+            }
+        });
 
-//         const data = await res.json();
-//         //console.log(req.nextUrl.pathname)
-//         if (!data.status && protectedRoutes.includes(req.nextUrl.pathname)) {
-//             console.log("Truw")
-//             const absoluteURL = new URL("/login", req.nextUrl.origin);
-//             return NextResponse.redirect(absoluteURL.toString());
-//         }
-// }
+        const data = await res.json();
+        //console.log(req.nextUrl.pathname)
+        if (!data.status && protectedRoutes.includes(req.nextUrl.pathname)) {
+            console.log("Truw")
+            const absoluteURL = new URL("/login", req.nextUrl.origin);
+            return NextResponse.redirect(absoluteURL.toString());
+        }
+}
