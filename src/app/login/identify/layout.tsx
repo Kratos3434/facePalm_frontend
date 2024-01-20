56be8b8d7714cc06472c5f392700714f5b9561df
William <william@inapulse.com>
2024-01-20 16:06:33 -0500

    Protect verify routes

--- src/app/login/identify/layout.tsx
+++ src/app/login/identify/layout.tsx
@@ -1,4 +1,7 @@
 import { Metadata } from "next";
+import { cookies } from "next/headers";
+import { baseURL } from "@/env";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "Forgot Password",
@@ -8,7 +11,31 @@ export const metadata: Metadata = {
 interface Props {
     children: React.ReactNode
 }
-const LoginLayout = ( { children }: Props ) => {
+
+const verifyIfLoggedIn = async () => {
+    const cookie = cookies();
+    const token = cookie.get('token')?.value;
+
+    if (token) {
+        const res = await fetch(`${baseURL}/admin/user/authenticate`, {
+            method: "GET",
+            headers: {
+                "Content-Type": 'application/json',
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const data = await res.json();
+
+        if (data.status) {
+            redirect("/");
+        }
+    }
+}
+
+const LoginLayout = async ( { children }: Props ) => {
+    await verifyIfLoggedIn();
+    
     return(
         <div className="tw-bg-[#F0F2F5]">
             {children}
