7ab1340b23e87eaf4062e641bb3e6d66a9a12453
keith carlos <keithcarlos34@gmail.com>
2024-01-21 19:01:34 -0500

    View Post Version 3

--- src/components/ViewPost.tsx
+++ src/components/ViewPost.tsx
@@ -16,8 +16,8 @@ const ViewPost = () => {
         view.post &&
         (
             <div className="tw-fixed tw-top-0 tw-w-full tw-h-full tw-overflow-auto tw-left-0 tw-bg-[rgb(0,0,0)] tw-bg-[rgba(0,0,0,0.4)] tw-py-5 tw-px-[16px] tw-z-[1000] tw-overflow-y-hidden" onClick={(e) => setView({ status: false, post: null })}>
-                <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-[100vh]">
-                    <div className="tw-rounded-md tw-shadow-xl tw-bg-white tw-max-w-[700px] tw-w-full tw-h-[1239px]" onClick={(e) => {
+                <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-max-h-[100vh] tw-h-full">
+                    <div className="tw-rounded-md tw-shadow-xl tw-bg-white tw-max-w-[700px] tw-w-full tw-h-full" onClick={(e) => {
                         e.stopPropagation()
                     }}>
                         <div className="tw-relative tw-text-center tw-p-2">
